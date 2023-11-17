991c2b7d46af3315d9ef02350f710a2dd7212041
Pahirathan Nithilan <60086422+Pahinithi@users.noreply.github.com>
2023-11-17 22:32:26 +0530

    Update models.py

--- movieticket/movieapp/models.py
+++ movieticket/movieapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+# Create your models here
 class movies(models.Model):
     name=models.CharField(max_length=50)
     image=models.ImageField(upload_to="pics",default="default.png")
