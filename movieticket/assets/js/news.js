b697a53fbbccf534c8e037655f8c82a27b62cee6
W.Robina <147932831+2001-01-01@users.noreply.github.com>
2023-11-29 04:39:15 +0530

    Update news.js

--- movieticket/assets/js/news.js
+++ movieticket/assets/js/news.js
@@ -1,16 +1,4 @@
-/* JS Document */
 
-/******************************
-
-[Table of Contents]
-
-1. Vars and Inits
-2. Set Header
-3. Init Menu
-4. Init Input
-
-
-******************************/
 
 $(document).ready(function()
 {
